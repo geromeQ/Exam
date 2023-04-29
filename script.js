d6e55c2baf3abcaf2b73442f72205372c4cd1810
geromeQ <127578170+geromeQ@users.noreply.github.com>
2023-04-29 08:58:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let quantity = 1;
+let price = 52000;
+let ship = 3000;
+
+function add() {
+	if(quantity >= 10) {
+		document.getElementById('num1').innerHTML = 10;
+		alert("Exceeded at available amount of stocks. " + quantity + ". That can be purchased.")
+	}
+	else {
+		let a = quantity + 1;
+		quantity = a
+		document.getElementById('num1').innerHTML = quantity;
+		document.getElementById('num2').innerHTML = quantity;
+
+		let nprice = quantity * price
+		let total = nprice + ship
+
+		document.getElementById('nprice').innerHTML = nprice;
+		document.getElementById('total').innerHTML = total;
+		document.getElementById('ship').innerHTML = ship;
+
+		document.getElementById('finalnum').innerHTML = quantity;
+		document.getElementById('finaltotal').innerHTML = total;
+		document.getElementById('finalship').innerHTML = ship;
+	}
+}
+
+function sub() {
+	if(quantity <= 1) {
+		document.getElementById('num1').innerHTML =1;
+		alert("There's no way you can buy negative amount of items")
+
+	}else{
+		let a = quantity - 1;
+		quantity = a
+		document.getElementById('num1').innerHTML = quantity;
+		document.getElementById('num2').innerHTML = quantity;
+
+		let nprice = quantity * price
+		let total = nprice + ship
+
+		document.getElementById('total').innerHTML = total;
+		document.getElementById('ship').innerHTML = ship;
+		document.getElementById('nprice').innerHTML = nprice;
+
+		document.getElementById('finalnum').innerHTML = quantity;
+		document.getElementById('finaltotal').innerHTML = total;
+		document.getElementById('finalship').innerHTML = ship;
+	}
+}
+
+function show1() {
+	document.getElementById('box1').style.display = 'none';
+	document.getElementById('box2').style.display = 'flex';
+}
+
+function show2() {
+	document.getElementById('box2').style.display = 'none';
+	document.getElementById('box4').style.display = 'flex';
+}
